b19e9052576ce0f88783cf2e99acfd1f7c424351
ThiaguinhoPE <soyunapersona451@gmail.com>
2025-06-08 14:42:38 -0500

    Add files via upload

---
+++ assets/preload/data/salvation/script.lua
@@ -0,0 +1,35 @@
+function onStepHit()
+	if curStep == 1 then
+		doTweenColor('bfColor', 'boyfriend', 'b4abb7', 0.01, 'linear');
+		doTweenColor('dadColor', 'dad', 'b4abb7', 0.01, 'linear');
+		doTweenColor('gfColor', 'gf', 'b4abb7', 0.01, 'linear');
+		doTweenColor('skyColor', 'BGSky', 'b4abb7', 0.01, 'linear');
+		doTweenColor('trainColor', 'BGTrain', 'b4abb7', 0.01, 'linear');
+		doTweenColor('floorColor', 'HOLY_floor', 'b4abb7', 0.01, 'linear');
+		doTweenColor('lampsColor', 'HOLY_BGLampLights', 'b4abb7', 0.01, 'linear');
+		doTweenColor('objectsColor', 'HOLY_objects', 'b4abb7', 0.01, 'linear');
+		doTweenColor('womanColor', 'HOLY_women', 'b4abb7', 0.01, 'linear');
+	end
+	if curStep == 2176 or curStep == 2944 then
+		doTweenColor('bfBlack', 'boyfriend', '000000', 0.01, 'linear');
+		doTweenColor('dadBlack', 'dad', '000000', 0.01, 'linear');
+		doTweenColor('gfBlack', 'gf', '000000', 0.01, 'linear');
+		doTweenColor('skyBlack', 'BGSky', '000000', 0.01, 'linear');
+		doTweenColor('trainBlack', 'BGTrain', 'ffffff', 0.01, 'linear');
+		doTweenColor('floorBlack', 'HOLY_floor', '000000', 0.01, 'linear');
+		doTweenColor('lampsBlack', 'HOLY_BGLampLights', '000000', 0.01, 'linear');
+		doTweenColor('objectsBlack', 'HOLY_objects', '000000', 0.01, 'linear');
+		doTweenColor('womanBlack', 'HOLY_women', '000000', 0.01, 'linear');
+	end
+	if curStep == 2432 or curStep == 3504 then
+		doTweenColor('bfColor', 'boyfriend', 'b4abb7', 4, 'linear');
+		doTweenColor('dadColor', 'dad', 'b4abb7', 4, 'linear');
+		doTweenColor('gfColor', 'gf', 'b4abb7', 4, 'linear');
+		doTweenColor('skyColor', 'BGSky', 'b4abb7', 4, 'linear');
+		doTweenColor('trainColor', 'BGTrain', 'b4abb7', 4, 'linear');
+		doTweenColor('floorColor', 'HOLY_floor', 'b4abb7', 4, 'linear');
+		doTweenColor('lampsColor', 'HOLY_BGLampLights', 'b4abb7', 4, 'linear');
+		doTweenColor('objectsColor', 'HOLY_objects', 'b4abb7', 4, 'linear');
+		doTweenColor('womanColor', 'HOLY_women', 'b4abb7', 4, 'linear');
+	end
+end
